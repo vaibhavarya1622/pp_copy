d48adc411af85bd8e9685771e106d512096978e5
vaibhavarya1622 <vaibhavarya1622@gmail.com>
2021-06-02 15:18:42 +0530

    s

--- src/components/HospitalList/FakeHospitalList.js
+++ src/components/HospitalList/FakeHospitalList.js
@@ -11,6 +11,7 @@ const hospitals=[
         name:"Sir SunderLal Hospital",
         city:"Banaras",
         district:"Banaras",
+        mobile:"9402930422",
         coords:[25.27658919429289, 82.99963237356269],
         distance:"3.4Km"
     },
@@ -18,6 +19,7 @@ const hospitals=[
         name:"Gyan Hospital",
         city:"Banaras",
         district:"Banaras",
+        mobile:"9402930422",
         coords:[25.27308551257164, 82.96728954590283],
         distance:"2.7Km"
     }
@@ -26,6 +28,7 @@ const hospitals=[
         name:"Arogya Hospital",
         city:"Banaras",
         district:"Banaras",
+        mobile:"9402930422",
         coords:[25.25888291540422, 82.96149842252024],
         distance:"6.5Km"
     }
